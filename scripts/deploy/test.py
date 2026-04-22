cc0a8cbeaae54693237b62b9b10685dabf6a76ea
Shreenija Reddy Daggavolu <dshreenija@gmail.com>
2026-04-22 00:42:06 -0500

    time

--- scripts/deploy/test.py
+++ scripts/deploy/test.py
@@ -1,3 +1,4 @@
+import time
 import boto3
 from botocore import UNSIGNED
 from botocore.client import Config
@@ -11,5 +12,11 @@ s3 = boto3.client(
     config=Config(signature_version=UNSIGNED),
 )
 
+start = time.perf_counter()
+
 obj = s3.get_object(Bucket=bucket, Key=key)
 print(obj["Body"].read().decode("utf-8"))
+
+end = time.perf_counter()
+
+print("S3 fetch latency (ms):", (end - start) * 1000)
